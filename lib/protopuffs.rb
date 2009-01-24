05c437e3a0031d58c6a2f2c7999971d6371955b0
Chris Kampmeier <chris@kampers.net>
2009-01-23 20:53:05 -0800

    Add a method to set the load path(s) where .proto files can be found

--- lib/protopuffs.rb
+++ lib/protopuffs.rb
@@ -1,2 +1,6 @@
+
 module Protopuffs
+  def self.proto_load_path
+    @proto_load_path ||= []
+  end
 end
--- test/protopuffs_test.rb
+++ test/protopuffs_test.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class ProtopuffsTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
+
+  context ".proto_load_path" do
+    should "be an accessor for an array of load paths for .proto files" do
+      Protopuffs.proto_load_path << "proto_files" << "other_proto_files"
+      assert_equal ["proto_files", "other_proto_files"], Protopuffs.proto_load_path
+    end
   end
+
 end
