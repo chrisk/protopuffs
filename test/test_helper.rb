ad4f04846cb920e477d54e45e067eee4cf9b74fe
Chris Kampmeier <chris@kampers.net>
2009-01-29 10:35:05 -0800

    Re-add lib/ to the load path in test_helper.rb so that each test file can be run individually without rake

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require 'test/unit'
 require 'shoulda'
 require 'mocha'
 require 'treetop'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'protopuffs'
 
 class Test::Unit::TestCase
