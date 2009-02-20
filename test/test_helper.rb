3b8560e8874bb8a13ae4263c8722a7641f3e6724
Chris Kampmeier <chris@kampers.net>
2009-02-19 22:51:29 -0800

    Add a handy test helper method for printing strings of bytes in binary and hex

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,13 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'protopuffs'
 
 class Test::Unit::TestCase
+
+  def print_bytes(string)
+    puts
+    string.each_byte do |byte|
+      printf("%1$08b (%1$02X)\n", byte)
+    end
+  end
+
 end
+
