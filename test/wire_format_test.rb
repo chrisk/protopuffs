3ed7bc59b31f401e87daed1cf17684aeddc08ccb
Chris Kampmeier <chris@kampers.net>
2009-02-19 23:05:30 -0800

    A little test cleanup

--- test/wire_format_test.rb
+++ test/wire_format_test.rb
@@ -9,10 +9,10 @@ class WireFormatTest < Test::Unit::TestCase
       fields = [Protopuffs::MessageField.new("required", "int32", "a", 1)]
       Protopuffs::MessageDescriptor.new("Test1", fields)
       @test1 = Protopuffs::Message::Test1.new
-      @test1.a = 150
     end
 
     should "serialize the message correctly when the field is set to 150" do
+      @test1.a = 150
       output = @test1.to_wire_format
       assert_equal 0x08, output[0]
       assert_equal 0x96, output[1]
@@ -26,11 +26,11 @@ class WireFormatTest < Test::Unit::TestCase
                 Protopuffs::MessageField.new("required", "int32", "b", 2)]
       Protopuffs::MessageDescriptor.new("Test1", fields)
       @test1 = Protopuffs::Message::Test1.new
-      @test1.a = 150
-      @test1.b = 157_372
     end
 
-    should "serialize the message correctly when the field is set to 150" do
+    should "serialize the message correctly when the fields are set to 150 and 157,372" do
+      @test1.a = 150
+      @test1.b = 157_372
       output = @test1.to_wire_format
       assert_equal 0x08, output[0]
       assert_equal 0x96, output[1]
