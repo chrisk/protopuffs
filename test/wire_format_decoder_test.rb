b59c3b8d10a97d0f47bdbfc620beeb13137bbf1f
Chris Kampmeier <chris@kampers.net>
2009-03-02 18:10:08 -0800

    Add a first failing test for the wire-format decoder

---
+++ test/wire_format_decoder_test.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class WireFormatDecoderTest < Test::Unit::TestCase
+
+  context "a message with one int32 field tagged 1" do
+    # from http://code.google.com/apis/protocolbuffers/docs/encoding.html#simple
+    setup do
+      fields = [Protopuffs::MessageField.new("required", "int32", "a", 1)]
+      Protopuffs::Message::Base.define_message_class("Test1", fields)
+      @message = Protopuffs::Message::Test1.new
+    end
+
+    should_decode_wire_format_to_fields [0x08, 0x96, 0x01], :a => 150
+  end
+
+end
