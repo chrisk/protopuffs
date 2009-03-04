f5e5e6e074c76d40ca0ce8d0f0506dffefd64cb6
Chris Kampmeier <chris@kampers.net>
2009-03-03 17:21:52 -0800

    Add more test coverage for decoding numeric varint fields

--- test/wire_format_decoder_test.rb
+++ test/wire_format_decoder_test.rb
@@ -16,4 +16,42 @@ class WireFormatDecoderTest < Test::Unit::TestCase
     should_decode_wire_format_to_fields [0x08, 0x96, 0x01, 0x10, 0xBC, 0xCD, 0x09], :a => 150
   end
 
+
+  context "a message with two int32 fields tagged #1 and #2" do
+    setup do
+      fields = [Protopuffs::MessageField.new("required", "int32", "a", 1),
+                Protopuffs::MessageField.new("required", "int32", "b", 2)]
+      Protopuffs::Message::Base.define_message_class("Test1", fields)
+      @message = Protopuffs::Message::Test1.new
+    end
+
+    should_decode_wire_format_to_fields [0x08, 0x96, 0x01, 0x10, 0xBC, 0xCD, 0x09],
+                                        :a => 150, :b => 157_372
+  end
+
+
+  context "a message with one int64 field tagged #1" do
+    setup do
+      fields = [Protopuffs::MessageField.new("required", "int64", "a", 1)]
+      Protopuffs::Message::Base.define_message_class("Test1", fields)
+      @message = Protopuffs::Message::Test1.new
+    end
+
+    should_decode_wire_format_to_fields [0x08, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x02],
+                                        :a => 2**50
+  end
+
+
+  context "a message with one uint32 field tagged #1 and one uint64 field tagged #2" do
+    setup do
+      fields = [Protopuffs::MessageField.new("required", "uint32", "a", 1),
+                Protopuffs::MessageField.new("required", "uint64", "b", 2)]
+      Protopuffs::Message::Base.define_message_class("Test1", fields)
+      @message = Protopuffs::Message::Test1.new
+    end
+
+    should_decode_wire_format_to_fields [0x08, 0x90, 0x07, 0x10, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x20],
+                                        :a => 912, :b => 2**54
+  end
+
 end
